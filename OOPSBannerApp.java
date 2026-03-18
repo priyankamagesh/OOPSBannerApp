586d5dfd4f1f6fb9e890c73d8d03d397195468f3
priyankamagesh <priyankamagesh9@gmail.com>
2026-03-18 12:07:16 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,10 +1,3 @@
-/**
- * OOPSBannerApp - Use Case 2
- * Prints the word "OOPS" in large banner format using '*' characters.
- *
- * @author Roshan
- * @version 2.0
- */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
