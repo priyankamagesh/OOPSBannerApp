e9b111e1ef2bbdbe78b3d840213833f59e5ad1d1
priyankamagesh <priyankamagesh9@gmail.com>
2026-03-18 12:06:32 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,10 +1,3 @@
-/**
- * OOPSBannerApp - Use Case 1
- * Prints the literal text "OOPS" to the console.
- *
- * @author Roshan
- * @version 1.0
- */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
