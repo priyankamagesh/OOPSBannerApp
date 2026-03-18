1037b9e31e582e1606902257078fbd741b788a23
priyankamagesh <priyankamagesh9@gmail.com>
2026-03-18 12:08:29 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,11 +1,3 @@
-/**
- * OOPSBannerApp - Use Case 4
- * Prints the word "OOPS" using String array and loop.
- * Improves modularity and reusability.
- *
- * @author Roshan
- * @version 4.0
- */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
